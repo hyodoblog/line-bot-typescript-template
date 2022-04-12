6ecb55a48f3859edc90348cc2918bd6e8252b3d4
hyodoblog <yusei.hyodo@youlib.co.jp>
2022-04-12 14:16:56 +0900

    firebase functionsのrouting設定

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,51 +1,8 @@
 import './alias'
 
 // ------------
-// firestore
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'firestoreDeleteAllCarts') {
-  exports.firestoreDeleteAllCarts = require('./triggers/firestore/delete-all-carts')
-}
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'firestoreCreateCustomerId') {
-  exports.firestoreCreateCustomerId = require('./triggers/firestore/create-customer-id')
-}
-
-// review
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'firestoreCreateCountProductReview') {
-  exports.firestoreCreateCountProductReview = require('./triggers/firestore/count-product-review/create')
-}
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'firestoreUpdateCountProductReview') {
-  exports.firestoreUpdateCountProductReview = require('./triggers/firestore/count-product-review/update')
-}
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'firestoreDeleteCountProductReview') {
-  exports.firestoreDeleteCountProductReview = require('./triggers/firestore/count-product-review/delete')
-}
-
-// ------------
-// pubsub
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'pubsubOrderCheckPayment') {
-  exports.pubsubOrderCheckPayment = require('./triggers/pubsub/order-check-payment')
-}
-
-// ------------
 // https
 
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'httpsServerAdmin') {
-  exports.httpsServerAdmin = require('./triggers/https/server-admin')
-}
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'httpsServerLiff') {
-  exports.httpsServerLiff = require('./triggers/https/server-liff')
-}
-
-// ------------
-// storage
-
-if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'storageCreate') {
-  exports.storageCreate = require('./triggers/storage/create')
+if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'lineBot') {
+  exports.lineBot = require('./triggers/https/line-bot')
 }
