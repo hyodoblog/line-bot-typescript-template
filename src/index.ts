541c7e3d10558cabf11ea36e9f18d9dc0898acc6
hyodoblog <yusei.hyodo@gmail.com>
2023-02-05 08:44:54 +0900

    fix path

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import './alias'
 // https
 
 if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'lineBot') {
-  exports.lineBot = require('./triggers/https/line-bot')
+  exports.lineBot = require('./funcs/line-bot')
 }
